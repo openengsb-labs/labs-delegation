4ee04969de037a2cfa2115aaab6a79c4ef98b1b4
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-03-20 15:29:51 +0100

    remove compendium-dependency

--- service/src/test/java/org/openengsb/labs/delegation/service/internal/FilterTest.java
+++ service/src/test/java/org/openengsb/labs/delegation/service/internal/FilterTest.java
@@ -47,7 +47,7 @@ import org.osgi.framework.Bundle;
 import org.osgi.framework.BundleContext;
 import org.osgi.framework.BundleEvent;
 import org.osgi.framework.BundleListener;
-import org.osgi.service.cm.Configuration;
+import org.slf4j.Logger;
 
 public class FilterTest {
 
@@ -88,8 +88,8 @@ public class FilterTest {
         Bundle bundle2 = mock(Bundle.class);
         BundleContext bundle2Context = mock(BundleContext.class);
         when(bundle2.getBundleContext()).thenReturn(bundle2Context);
-        when(bundle2.getHeaders()).thenReturn(new Hashtable<String, Object>());
-        Dictionary<String, Object> headers = new Hashtable<String, Object>();
+        when(bundle2.getHeaders()).thenReturn(new Hashtable<String, String>());
+        Dictionary<String, String> headers = new Hashtable<String, String>();
         headers.put(Constants.PROVIDED_CLASSES, "*");
         when(bundle2.getHeaders()).thenReturn(headers);
         installBundle(bundle2);
@@ -103,10 +103,10 @@ public class FilterTest {
         Bundle bundle2 = mock(Bundle.class);
         BundleContext bundle2Context = mock(BundleContext.class);
         when(bundle2.getBundleContext()).thenReturn(bundle2Context);
-        Dictionary<String, Object> headers = new Hashtable<String, Object>();
+        Dictionary<String, String> headers = new Hashtable<String, String>();
         headers.put(Constants.PROVIDED_CLASSES, "*");
         when(bundle2.getHeaders()).thenReturn(headers);
-        when(bundle2.loadClass(Bundle.class.getName())).thenReturn(Bundle.class);
+        when(bundle2.loadClass(Bundle.class.getName())).thenReturn((Class) Bundle.class);
         ArgumentCaptor<ClassProvider> captor = ArgumentCaptor.forClass(ClassProvider.class);
         installBundle(bundle2);
         verify(bundle2Context).registerService(eq(ClassProvider.class.getName()), captor.capture(),
@@ -115,22 +115,23 @@ public class FilterTest {
         assertThat(provider.loadClass(Bundle.class.getName()), equalTo((Class) Bundle.class));
     }
 
+    @SuppressWarnings("rawtypes")
     @Test
     public void installBundleWithHeaders_shouldProvideOnlySpecificClasses() throws Exception {
         Bundle bundle2 = mock(Bundle.class);
         BundleContext bundle2Context = mock(BundleContext.class);
         when(bundle2.getBundleContext()).thenReturn(bundle2Context);
-        Dictionary<String, Object> headers = new Hashtable<String, Object>();
+        Dictionary<String, String> headers = new Hashtable<String, String>();
         headers.put(Constants.PROVIDED_CLASSES, "org.osgi.framework.*");
         when(bundle2.getHeaders()).thenReturn(headers);
-        when(bundle2.loadClass(Configuration.class.getName())).thenReturn(Configuration.class);
+        when(bundle2.loadClass(Logger.class.getName())).thenReturn((Class) Logger.class);
         ArgumentCaptor<ClassProvider> captor = ArgumentCaptor.forClass(ClassProvider.class);
         installBundle(bundle2);
         verify(bundle2Context).registerService(eq(ClassProvider.class.getName()), captor.capture(),
             any(Dictionary.class));
         ClassProvider provider = captor.getValue();
         try {
-            provider.loadClass(Configuration.class.getName());
+            provider.loadClass(Logger.class.getName());
             fail("Expected class not to be found");
         } catch (ClassNotFoundException e) {
             // expected
@@ -143,11 +144,11 @@ public class FilterTest {
         Bundle bundle2 = mock(Bundle.class);
         BundleContext bundle2Context = mock(BundleContext.class);
         when(bundle2.getBundleContext()).thenReturn(bundle2Context);
-        when(bundle2.loadClass(Configuration.class.getName())).thenReturn(Configuration.class);
-        when(bundle2.getHeaders()).thenReturn(new Hashtable<String, Object>());
+        when(bundle2.loadClass(Logger.class.getName())).thenReturn((Class) Logger.class);
+        when(bundle2.getHeaders()).thenReturn(new Hashtable<String, String>());
         installBundle(bundle2);
         ClassProvider provider = DelegationUtil.registerClassProviderForBundle(bundle2);
-        assertThat(provider.loadClass(Configuration.class.getName()), equalTo((Class) Configuration.class));
+        assertThat(provider.loadClass(Logger.class.getName()), equalTo((Class) Logger.class));
     }
 
     private void installBundle(Bundle bundle2) {

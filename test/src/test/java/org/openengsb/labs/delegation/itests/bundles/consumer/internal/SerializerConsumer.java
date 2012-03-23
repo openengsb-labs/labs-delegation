f38f546716c9bcbde32ee1cb03d8bd02622a18b7
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-03-23 12:28:02 +0100

    indicate that the test fails because of different classloaders

--- test/src/test/java/org/openengsb/labs/delegation/itests/bundles/consumer/internal/SerializerConsumer.java
+++ test/src/test/java/org/openengsb/labs/delegation/itests/bundles/consumer/internal/SerializerConsumer.java
@@ -1,8 +1,5 @@
 package org.openengsb.labs.delegation.itests.bundles.consumer.internal;
 
-import static org.hamcrest.CoreMatchers.equalTo;
-import static org.junit.Assert.assertThat;
-
 import java.io.ByteArrayInputStream;
 import java.io.ByteArrayOutputStream;
 import java.io.ObjectInputStream;
@@ -19,18 +16,22 @@ public class SerializerConsumer implements BundleActivator {
         ServiceReference reference = context.getServiceReferences(Callable.class.getName(), "(type=pageProvider)")[0];
         @SuppressWarnings("unchecked")
         Callable<Object> provider = (Callable<Object>) context.getService(reference);
-        Object orignal = provider.call();
+        Object original = provider.call();
         ByteArrayOutputStream out = new ByteArrayOutputStream();
-        new ObjectOutputStream(out).writeObject(orignal);
+        new ObjectOutputStream(out).writeObject(original);
         byte[] data = out.toByteArray();
 
         ObjectInputStream ois = new ObjectInputStream(new ByteArrayInputStream(data));
         Object pageObject = ois.readObject();
-        if (!pageObject.equals(orignal)) {
-            throw new Exception("not equal");
+        if (!pageObject.equals(original)) {
+            throw new Exception(String.format("object was not equal after deserializing\n" +
+                    "%s != %s\n" +
+                    "%s <-> %s", pageObject, original, pageObject.getClass().getClassLoader(), original.getClass()
+                .getClassLoader()));
         }
     }
 
+    @Override
     public void stop(BundleContext context) throws Exception {
     };
 

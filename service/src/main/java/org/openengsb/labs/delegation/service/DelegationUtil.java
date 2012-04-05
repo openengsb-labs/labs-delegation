d395eac8d0c36beadb10fe6ae9fc992969eab0c0
Christoph Gritschenberger <christoph.gritschenberger@gmail.com>
2012-04-05 17:28:14 +0200

    refactor DelegationUtil and Activator-implementation

--- service/src/main/java/org/openengsb/labs/delegation/service/DelegationUtil.java
+++ service/src/main/java/org/openengsb/labs/delegation/service/DelegationUtil.java
@@ -17,16 +17,9 @@
 
 package org.openengsb.labs.delegation.service;
 
-import java.net.URL;
 import java.util.Collection;
-import java.util.HashSet;
-import java.util.Hashtable;
-import java.util.LinkedList;
-import java.util.Set;
-import java.util.regex.Pattern;
 
-import org.openengsb.labs.delegation.service.internal.Activator;
-import org.openengsb.labs.delegation.service.internal.ClassloadingDelegateImpl;
+import org.openengsb.labs.delegation.service.internal.BundleHandler;
 import org.osgi.framework.Bundle;
 
 public final class DelegationUtil {
@@ -34,16 +27,16 @@ public final class DelegationUtil {
     /**
      * registers a ClassProvider service that is able to load all classes contained in the given bundle
      */
-    public static ClassProvider registerClassProviderForBundle(Bundle b) {
-        return doRegisterClassProviderForBundle(b, Activator.discoverClasses(b));
+    public static void registerClassProviderForBundle(Bundle bundle) {
+        BundleHandler.injectBundle(bundle);
     }
 
     /**
      * registers a ClassProvider service with the given delegationContext that is able to load all classes contained in
      * the given bundle
      */
-    public static ClassProvider registerClassProviderForBundle(Bundle b, String delegationContext) {
-        return doRegisterClassProviderForBundle(b, Activator.discoverClasses(b), delegationContext);
+    public static void registerClassProviderForBundle(Bundle bundle, String delegationContext) {
+        BundleHandler.injectBundle(bundle, delegationContext);
     }
 
     /**
@@ -54,10 +47,8 @@ public final class DelegationUtil {
      * 
      * Example: my.bundle.mainpackage, my.bundle.otherpackage.*
      */
-    public static ClassProvider registerClassProviderForBundle(Bundle b, Collection<String> classFilter) {
-        Collection<String> discoveredClasses = Activator.discoverClasses(b);
-        Set<String> matchingClasses = getMatchingClasses(classFilter, discoveredClasses);
-        return doRegisterClassProviderForBundle(b, matchingClasses);
+    public static void registerClassProviderForBundle(Bundle bundle, Collection<String> classFilters) {
+        BundleHandler.injectBundle(bundle, classFilters);
     }
 
     /**
@@ -68,63 +59,9 @@ public final class DelegationUtil {
      * 
      * Example: my.bundle.mainpackage, my.bundle.otherpackage.*
      */
-    public static ClassProvider registerClassProviderForBundle(Bundle b, Collection<String> classFilter,
+    public static void registerClassProviderForBundle(Bundle bundle, Collection<String> classFilters,
             String delegationContext) {
-        Collection<String> discoveredClasses = Activator.discoverClasses(b);
-        Set<String> matchingClasses = getMatchingClasses(classFilter, discoveredClasses);
-        return doRegisterClassProviderForBundle(b, matchingClasses, delegationContext);
-    }
-
-    private static ClassProvider doRegisterClassProviderForBundle(Bundle b, Set<String> classes) {
-        ClassProvider service = new ClassloadingDelegateImpl(b, classes);
-        Hashtable<String, Object> properties = new Hashtable<String, Object>();
-        properties.put(Constants.PROVIDED_CLASSES_KEY, classes);
-        b.getBundleContext().registerService(ClassProvider.class.getName(), service, properties);
-        return service;
-    }
-
-    private static ClassProvider doRegisterClassProviderForBundle(Bundle b, Set<String> classes,
-            String delegationContext) {
-        ClassProvider service = new ClassloadingDelegateImpl(b, classes);
-        Hashtable<String, Object> properties = new Hashtable<String, Object>();
-        properties.put(Constants.PROVIDED_CLASSES_KEY, classes);
-        properties.put(Constants.DELEGATION_CONTEXT, delegationContext);
-        b.getBundleContext().registerService(ClassProvider.class.getName(), service, properties);
-        return service;
-    }
-
-    private static Set<String> getMatchingClasses(Collection<String> classFilters, Collection<String> allClasses) {
-        Set<String> matchingClasses = new HashSet<String>();
-        Collection<String> expressions = prepareFilterExpressions(classFilters);
-        for (String classname : allClasses) {
-            for (String e : expressions) {
-                if (Pattern.matches(e, classname)) {
-                    matchingClasses.add(classname);
-                    break;
-                }
-            }
-        }
-        return matchingClasses;
-    }
-
-    private static Collection<String> prepareFilterExpressions(Collection<String> classFilters) {
-        Collection<String> expressions = new LinkedList<String>();
-        for (String cFilter : classFilters) {
-            expressions.add(
-                cFilter
-                    .replaceAll("\\.", "\\.")
-                    .replaceAll("\\*", ".*")
-                );
-        }
-        return expressions;
-    }
-
-    public static String extractClassName(URL classURL) {
-        String path = classURL.getPath();
-        return path
-            .replaceAll("^/", "")
-            .replaceAll(".class$", "")
-            .replaceAll("\\/", ".");
+        BundleHandler.injectBundle(bundle, classFilters, delegationContext);
     }
 
     private DelegationUtil() {
---
+++ service/src/main/java/org/openengsb/labs/delegation/service/internal/BundleHandler.java
@@ -0,0 +1,225 @@
+package org.openengsb.labs.delegation.service.internal;
+
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Enumeration;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Hashtable;
+import java.util.LinkedList;
+import java.util.Map;
+import java.util.Set;
+import java.util.regex.Pattern;
+
+import org.openengsb.labs.delegation.service.ClassProvider;
+import org.openengsb.labs.delegation.service.Constants;
+import org.openengsb.labs.delegation.service.Provide;
+import org.osgi.framework.Bundle;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class BundleHandler {
+
+    private static final Logger LOGGER = LoggerFactory.getLogger(BundleHandler.class);
+
+    private Bundle bundle;
+    private Map<String, Set<String>> providedClassesMap = new HashMap<String, Set<String>>();
+
+    private Set<String> bundleClasses;
+
+    public static void processBundle(Bundle bundle) {
+        BundleHandler bundleHandler = new BundleHandler(bundle);
+        bundleHandler.scanBundle();
+        bundleHandler.handle();
+    }
+
+    public static void injectBundle(Bundle bundle, String context) {
+        BundleHandler bundleHandler = new BundleHandler(bundle);
+        doRegisterClassProviderForBundle(bundle, bundleHandler.getAllClassesInBundle(), context);
+    }
+
+    public static void injectBundle(Bundle bundle) {
+        injectBundle(bundle, "");
+    }
+
+    public static void injectBundle(Bundle bundle, Collection<String> classFilters, String context) {
+        BundleHandler bundleHandler = new BundleHandler(bundle);
+        Set<String> matchingClasses = bundleHandler.getMatchingClasses(classFilters);
+        doRegisterClassProviderForBundle(bundle, matchingClasses, context);
+    }
+
+    public static void injectBundle(Bundle bundle, Collection<String> classFilters) {
+        injectBundle(bundle, classFilters, "");
+    }
+
+    private BundleHandler(Bundle bundle) {
+        this.bundle = bundle;
+    }
+
+    private BundleHandler(Bundle bundle, Map<String, Set<String>> providedClassesMap) {
+        this.bundle = bundle;
+        this.providedClassesMap = providedClassesMap;
+    }
+
+    public void scanBundle() {
+        providedClassesMap.clear();
+        analyzePlainProvidesHeader();
+        analyzeProvidesHeadersWithContext();
+        analyzeAnnotations();
+    }
+
+    public void handle() {
+        LOGGER.info("injecting ClassProvider-Service into bundle {}.", bundle.getSymbolicName());
+        if (providedClassesMap.containsKey("")) {
+            Set<String> allClasses = providedClassesMap.remove("");
+            doRegisterClassProviderForBundle(bundle, allClasses);
+        }
+        for (Map.Entry<String, Set<String>> entry : providedClassesMap.entrySet()) {
+            doRegisterClassProviderForBundle(bundle, entry.getValue(), entry.getKey());
+        }
+    }
+
+    private void analyzeAnnotations() {
+        if (bundle.getHeaders().get(Constants.DELEGATION_ANNOTATIONS) == null) {
+            return;
+        }
+        Set<String> discoverClasses = getAllClassesInBundle();
+        for (String classname : discoverClasses) {
+            Class<?> clazz;
+            try {
+                clazz = bundle.loadClass(classname);
+            } catch (ClassNotFoundException e) {
+                LOGGER.warn("bundle could not find own class: " + classname, e);
+                continue;
+            }
+            Provide provide = clazz.getAnnotation(Provide.class);
+            if (provide != null) {
+                addClassToContext("", classname);
+                for (String context : provide.value()) {
+                    addClassToContext(context, classname);
+                }
+            }
+        }
+    }
+
+    private void analyzeProvidesHeadersWithContext() {
+        @SuppressWarnings("unchecked")
+        Enumeration<String> keys = bundle.getHeaders().keys();
+        while (keys.hasMoreElements()) {
+            String key = keys.nextElement();
+            if (key.startsWith(Constants.PROVIDED_CLASSES + "-")) {
+                String context = key.replaceFirst(Constants.PROVIDED_CLASSES + "\\-", "");
+                String providedClassesString = (String) bundle.getHeaders().get(key);
+                Collection<String> providedClasses = parseProvidedClasses(providedClassesString);
+                Set<String> matchingClasses = getMatchingClasses(providedClasses);
+                addClassesToContext(context, matchingClasses);
+            }
+        }
+    }
+
+    private void analyzePlainProvidesHeader() {
+        String providedClassesString = (String) bundle.getHeaders().get(Constants.PROVIDED_CLASSES);
+        if (providedClassesString == null || providedClassesString.isEmpty()) {
+            return;
+        }
+        Collection<String> providedClasses = parseProvidedClasses(providedClassesString);
+        Set<String> matchingClasses = getMatchingClasses(providedClasses);
+        addClassesToContext("", matchingClasses);
+    }
+
+    private Collection<String> parseProvidedClasses(String providedClassesString) {
+        String[] providedClassesArray = providedClassesString.split(",");
+        Collection<String> providedClassesList = new ArrayList<String>();
+        for (String p : providedClassesArray) {
+            providedClassesList.add(p.trim());
+        }
+        return providedClassesList;
+    }
+
+    private void addClassToContext(String context, String clazz) {
+        if (!providedClassesMap.containsKey(context)) {
+            providedClassesMap.put(context, new HashSet<String>());
+        }
+        providedClassesMap.get(context).add(clazz);
+    }
+
+    private void addClassesToContext(String context, Set<String> classes) {
+        if (!providedClassesMap.containsKey(context)) {
+            providedClassesMap.put(context, classes);
+        } else {
+            providedClassesMap.get(context).addAll(classes);
+        }
+    }
+
+    private static ClassProvider doRegisterClassProviderForBundle(Bundle b, Set<String> classes) {
+        ClassProvider service = new ClassloadingDelegateImpl(b, classes);
+        Hashtable<String, Object> properties = new Hashtable<String, Object>();
+        properties.put(Constants.PROVIDED_CLASSES_KEY, classes);
+        b.getBundleContext().registerService(ClassProvider.class.getName(), service, properties);
+        return service;
+    }
+
+    private static ClassProvider doRegisterClassProviderForBundle(Bundle b, Set<String> classes,
+            String delegationContext) {
+        ClassProvider service = new ClassloadingDelegateImpl(b, classes);
+        Hashtable<String, Object> properties = new Hashtable<String, Object>();
+        properties.put(Constants.PROVIDED_CLASSES_KEY, classes);
+        properties.put(Constants.DELEGATION_CONTEXT, delegationContext);
+        b.getBundleContext().registerService(ClassProvider.class.getName(), service, properties);
+        return service;
+    }
+
+    private Set<String> getMatchingClasses(Collection<String> classFilters) {
+        Set<String> matchingClasses = new HashSet<String>();
+        Collection<String> expressions = prepareFilterExpressions(classFilters);
+        for (String classname : getAllClassesInBundle()) {
+            for (String e : expressions) {
+                if (Pattern.matches(e, classname)) {
+                    matchingClasses.add(classname);
+                    break;
+                }
+            }
+        }
+        return matchingClasses;
+    }
+
+    private Set<String> getAllClassesInBundle() {
+        if (bundleClasses == null) {
+            bundleClasses = discoverClasses(bundle);
+        }
+        return bundleClasses;
+    }
+
+    private static Collection<String> prepareFilterExpressions(Collection<String> classFilters) {
+        Collection<String> expressions = new LinkedList<String>();
+        for (String cFilter : classFilters) {
+            expressions.add(
+                cFilter
+                    .replaceAll("\\.", "\\.")
+                    .replaceAll("\\*", ".*")
+                );
+        }
+        return expressions;
+    }
+
+    private static String extractClassName(URL classURL) {
+        String path = classURL.getPath();
+        return path
+            .replaceAll("^/", "")
+            .replaceAll(".class$", "")
+            .replaceAll("\\/", ".");
+    }
+
+    private static Set<String> discoverClasses(Bundle bundle) {
+        @SuppressWarnings("unchecked")
+        Enumeration<URL> classEntries = bundle.findEntries("/", "*.class", true);
+        Set<String> discoveredClasses = new HashSet<String>();
+        while (classEntries.hasMoreElements()) {
+            URL classURL = classEntries.nextElement();
+            String className = extractClassName(classURL);
+            discoveredClasses.add(className);
+        }
+        return discoveredClasses;
+    }
+}
